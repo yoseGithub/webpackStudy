51535e62f820f68645330eaa89ff0334162aacab
ice <1764420633@qq.com>
2019-05-08 00:51:21 +0800

    section-2.1

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@
 import Header from './header.js'
 import Content from './content.js'
 import Sidebar from './sidebar.js'
+import avatar from './avatar.jpg'
 
 let root = document.getElementById("root")
 
 let header = new Header(root)
 let content = new Content(root)
 let sidebar = new Sidebar(root)
+
+let img = new Image()
+img.src = avatar
+root.append(img)
--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,20 @@ const path = require('path');  //引入node 的 path 核心模块
 
 /* commonJS语法 */
 module.exports = {
+    mode: "none",
     entry: './src/index.js',  //入口文件
     output: {
         filename: 'bundle.js',  //文件名
         path: path.resolve(__dirname, 'dist')  //__dirname指的是webpack.config.js这个文件所在的路径
     },
-    mode: "none"
+    module: {
+        rules: [  //模块打包规则
+            {
+                test: /\.jpg$/,
+                use: {
+                    loader: 'file-loader'
+                }
+            }
+        ]
+    }
 }
