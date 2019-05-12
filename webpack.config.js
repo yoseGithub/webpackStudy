2fb4b19ab27cec2ad943951cd54698647e22ce3f
ice <1764420633@qq.com>
2019-05-12 19:37:58 +0800

    section-2.5

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');  //引入node 的 path 核心模块
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 /* commonJS语法 */
 module.exports = {
@@ -22,8 +24,7 @@ module.exports = {
                         outputPath: 'icon-font/'         //打包到css文件夹下，如果没有会创建该文件夹
                     }
                 }
-            },
-            {
+            },{
                 test: /\.(jpg|png|gif)$/,
                 use: {
                     // 会把图片返回成base64位文件，省了HTTP请求，但是增加了js大小
@@ -35,17 +36,7 @@ module.exports = {
                         limit: 2048  //是否小于2KB
                     }
                 }
-            },
-            // {
-            //     test: /\.css$/,
-            //     //css需要同时使用两个loader，css-loader负责简析css语法，style-loader负责把样式添加到head上
-            //     use: ['style-loader', 'css-loader']
-            // },
-            // 安装cnpm - npm install -g cnpm --registry=https://registry.npm.taobao.org
-            // 安装cnpm i sass-loader node-sass -D
-            // autoprefixer 自动给属性加前缀
-            // cnpm i autoprefixer -D
-            {
+            },{
                 test: /\.scss$/,
                 use: [
                     'style-loader',
@@ -61,5 +52,11 @@ module.exports = {
                     'postcss-loader']
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html'      // 以index.html作为html模板
+        }),
+        new CleanWebpackPlugin()         // 打包之前删除dist目录，现在的新版是直接取output路径，不需要自己来写删除哪个打包后的文件
+    ]
 }
