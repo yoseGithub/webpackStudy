1c29f87f4255da06f3016659a5c94b8cbd942540
ice <1764420633@qq.com>
2019-05-19 12:55:31 +0800

    section-2.11&2.12

--- src/index.js
+++ src/index.js
@@ -1,35 +1,12 @@
-import Count from './counter'
-import Num from './num'
-import './style.css'
+// import "@babel/polyfill";
+// import "core-js/stable";
+// import "regenerator-runtime/runtime";
 
-let root = document.getElementById("root")
-let countNum = new Count();
-let num = new Num();
+const arr = [
+    new Promise(() => {}),
+    new Promise(() => {})
+];
 
-let countElm = document.createElement('div')
-
-let btn = document.createElement('button')
-btn.innerHTML = '新增'
-root.appendChild(btn)
-root.appendChild(countElm)
-root.appendChild(num.block)
-
-btn.onclick = function() {
-    countNum.add()
-    countElm.innerHTML = countNum.num
-
-    let div = document.createElement('div')
-    div.innerHTML = 'item'
-    root.appendChild(div)
-}
-
-if(module.hot) {
-    module.hot.accept('./num', () => {
-        // console.log(num)
-        // num.reset()
-        
-        root.removeChild(num.block)
-        num = new Num();
-        root.appendChild(num.block)
-    })
-}
+arr.map(item => {
+    console.log(item)
+})
