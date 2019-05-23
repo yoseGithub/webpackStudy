94b0c15b4ae15f72f76bf701205fac45cb746618
ice <1764420633@qq.com>
2019-05-24 00:37:17 +0800

    section-3.8

---
+++ src/click.js
@@ -0,0 +1,7 @@
+function handleClick () {
+    const elm = document.createElement('div');
+    elm.innerHTML = "Dell-Lee"
+    document.getElementById('root').appendChild(elm);
+}
+
+export default handleClick;
